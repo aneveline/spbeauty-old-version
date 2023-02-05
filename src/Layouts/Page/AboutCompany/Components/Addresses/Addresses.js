56e7dfc3f86abea235646777c2348f91f85a7d40
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:06:12 +0000

    d-flex

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,13 +2,13 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex justify-content-between flex-column gap-4 mt-4 col' class="addresses">
+        <div className='d-flex justify-content-between flex-column gap-4 mt-4 col' class="addresses">
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+            <div className='d-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
-            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
