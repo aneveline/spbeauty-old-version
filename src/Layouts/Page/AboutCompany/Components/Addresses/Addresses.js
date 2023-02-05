9648194da5700847998e5742021f85df9520825f
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:13:04 +0000

    flex-start

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,11 +2,11 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-flex justify-content-between flex-column gap-4 mt-4 col' class="addresses">
+        <div className='d-inline-flex justify-content-between flex-column gap-4 mt-4 col' class="addresses">
             <div className='title'>Адреса заведений</div>
-            <div className='d-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+            <div className='flex-start flex-column gap-2'>
+                <ListElement className='gap-3'>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                <ListElement className='gap-3'>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
         </div>
