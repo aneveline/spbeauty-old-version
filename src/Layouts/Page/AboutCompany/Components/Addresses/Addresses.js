0f5b5eb3f02b88583d8eb66c02775dfdcbe18e09
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:16:52 +0000

    justify-content-between

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,7 +2,7 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex justify-content-between flex-column gap-4 mt-4 col' class="addresses">
+        <div className='justify-content-between d-inline-flex flex-column gap-4 col' class="addresses">
             <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-2'>
                 <ListElement className='gap-3'>Санкт-Петербург, Невский пр-т. 28</ListElement>
