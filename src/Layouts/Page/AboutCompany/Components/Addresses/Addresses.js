fb2c5393945860789a6c6cf3d85b1ab4e8e944bf
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:20:54 +0000

    return

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,10 +2,11 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='justify-content-between d-inline-flex flex-column gap-4 mt-4 col' class="addresses">
-            <div className='title d-inline-flex flex-column gap-2'>Адреса заведений
-                <ListElement className='gap-3'>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement className='gap-3'>Санкт-Петербург, Казанская ул, 7</ListElement>
+        <div className='d-inline-flex justify-content-between flex-column gap-4 mt-4 col'>
+            <div className='title'>Адреса заведений</div>
+            <div className='d-inline-flex flex-column gap-3'>
+                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
         </div>
