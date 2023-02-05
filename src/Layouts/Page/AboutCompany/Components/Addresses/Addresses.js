49a9dd32febd715c174778c8131aac4f0ed69088
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:00:47 +0000

    secondary button out

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -7,8 +7,8 @@ function Addresses() {
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
