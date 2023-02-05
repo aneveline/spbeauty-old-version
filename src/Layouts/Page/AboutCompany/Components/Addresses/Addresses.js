b0f8843ab1ca00688203062d77d6a6e06ba611ba
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:52:14 +0000

    - internal div

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,13 +2,13 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='addresses d-inline-flex flex-column gap-4 mt-4 col' >
-            <div className='justify-content-between d-flex flex-column gap-3'>
+        <div className='addresses justify-content-between d-inline-flex flex-column gap-4 mt-4 col' >
+            
                 <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
-            </div>
+            
         </div>
     );
 }
