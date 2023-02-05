27d69abb14bf16bde6414dae08a02b9b4d695f66
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-05 16:31:48 +0000

    flex-start

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,7 +4,7 @@ function Addresses() {
     return (
         <div className='addresses d-inline-flex justify-content-between flex-column gap-4 mt-4 col' >
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+            <div className='flex-start flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
